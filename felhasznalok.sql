1ecf3ca857a39f0c7cf1137933ba83296ba39b86
Reibladam <109725060+Reibladam@users.noreply.github.com>
2025-03-06 20:18:16 +0100

    adatbázis felhasználók és könyvek táblával
    az adatbázist létrehozó sql fájl feltöltve

---
+++ felhasznalok.sql
@@ -0,0 +1,104 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Gép: 127.0.0.1
+-- Létrehozás ideje: 2025. Már 06. 20:16
+-- Kiszolgáló verziója: 10.4.32-MariaDB
+-- PHP verzió: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Adatbázis: `felhasznalok`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Tábla szerkezet ehhez a táblához `adatok`
+--
+
+CREATE TABLE `adatok` (
+  `id` int(11) NOT NULL,
+  `felhasznalonev` varchar(200) NOT NULL,
+  `email` varchar(200) NOT NULL,
+  `jelszo` text NOT NULL,
+  `megerositve` bit(1) NOT NULL,
+  `token` text DEFAULT NULL,
+  `reset_token` varchar(255) DEFAULT NULL,
+  `reset_token_expiry` datetime DEFAULT NULL,
+  `remember_token` text DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_hungarian_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Tábla szerkezet ehhez a táblához `konyvek`
+--
+
+CREATE TABLE `konyvek` (
+  `id` int(11) NOT NULL,
+  `cim` varchar(200) NOT NULL,
+  `leiras` text NOT NULL,
+  `szerzo` varchar(200) NOT NULL,
+  `kiadas` varchar(4) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `borito` text DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_hungarian_ci;
+
+--
+-- Indexek a kiírt táblákhoz
+--
+
+--
+-- A tábla indexei `adatok`
+--
+ALTER TABLE `adatok`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- A tábla indexei `konyvek`
+--
+ALTER TABLE `konyvek`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user_id` (`user_id`);
+
+--
+-- A kiírt táblák AUTO_INCREMENT értéke
+--
+
+--
+-- AUTO_INCREMENT a táblához `adatok`
+--
+ALTER TABLE `adatok`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT a táblához `konyvek`
+--
+ALTER TABLE `konyvek`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Megkötések a kiírt táblákhoz
+--
+
+--
+-- Megkötések a táblához `konyvek`
+--
+ALTER TABLE `konyvek`
+  ADD CONSTRAINT `konyvek_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `adatok` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
